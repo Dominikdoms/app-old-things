ec4b5f9f4079c594db1243bf4d35bbc4882fc682
Dominik <d.rolewski98@gmail.com>
2021-03-09 22:28:29 +0100

    add background in section tree column

--- src/components/home/HomeThreeColumns.js
+++ src/components/home/HomeThreeColumns.js
@@ -1,8 +1,33 @@
 import React from "react";
+import {useState} from "react"
+import "./homeThreeColumns.scss"
 
 export const HomeThreeColumns = () => {
+    const [bags, setBags] = useState("10")//fetch
+    const [organizations, setOrganizations] = useState("5")//fetch
+    const [collections, setCollections] = useState("7")//fetch
 
-    return(
-        <h1>HomeThreeColumns</h1>
+    return (
+        <>
+            <section className={"home__tree-column"}>
+                <div className={"home__column"}>
+                    <h2 className={"home__count"}>{bags}</h2>
+                    <h3 className={"home__header"}>ODDANYCH WORKÓW</h3>
+                    <p className={"home__text"}>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+
+                <div className={"home__column"}>
+                    <h2>{organizations}</h2>
+                    <h3>WSPARTYCH ORGANIZACJI</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+
+                <div className={"home__column"}>
+                    <h2>{collections}</h2>
+                    <h3>ZORGANIZOWANY ZBIÓREK</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+            </section>
+        </>
     )
 }
