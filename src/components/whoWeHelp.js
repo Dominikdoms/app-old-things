e9fbe03b6b6593ed0069734ed897dd4e40645fae
Dominik <d.rolewski98@gmail.com>
2021-03-10 22:35:26 +0100

    creating new section

--- src/components/whoWeHelp.js
+++ src/components/whoWeHelp.js
@@ -1,8 +1,31 @@
-import React from "react";
-import {Link, Switch, Route} from "react-router-dom"
+import React, {useState, useEffect} from "react";
+import clx from 'classnames';
 import './whoWeHelp.scss'
 
 export const WhoWeHelp = () => {
+    const API = "http://localhost:3000/"
+    const [organizations, setOrganizations] = useState([]);
+    const [current, setCurrent] = useState('fundation');
+    const [fundations, setFundations] = useState('')
+
+    const handleCurrent = e => {
+        const {id} = e.target;
+        setCurrent(id)
+    }
+
+    useEffect(() => {
+        fetch(`${API}${current}`)
+            .then(response => response.json())
+            .then(data => {
+                setOrganizations(data)
+            })
+            .catch(err => console.log(err))
+
+        if (current === "fundation") setFundations("Fundacja")
+        if (current === "organization") setFundations("Organizacja")
+        if (current === "collection") setFundations("Zbiórka")
+    }, [current])
+
 
     return (
         <section className={"fundations"} id="fundations">
@@ -10,20 +33,39 @@ export const WhoWeHelp = () => {
                 <h2 className={"fundations__header"}>Komu pomagamy?</h2>
                 <nav>
                     <ul className={"fundations__nav"}>
-                        <li><button><Link to="/fundacje">Fundacjom</Link></button></li>
-                        <li><button><Link to="/organizacje">Organizacjom <br/> Pozarządowym</Link></button></li>
-                        <li className={"fundation__btn"}><button><Link to="/zbiorki">Lokalnym <br/> zbiórkom</Link></button></li>
+                        <li>
+                            <button className={clx({active: current === "fundation"})} id="fundation"
+                                    onClick={handleCurrent}>Fundacjom
+                            </button>
+                        </li>
+                        <li>
+                            <button className={clx({active: current === "organization"})} id="organization"
+                                    onClick={handleCurrent}>Organizacjom <br/> Pozarządowym
+                            </button>
+                        </li>
+                        <li>
+                            <button className={clx({active: current === "collection"})} id="collection"
+                                    onClick={handleCurrent}>Lokalnym <br/> zbiórkom
+                            </button>
+                        </li>
                     </ul>
                 </nav>
                 <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić
                     czym
                     się zajmują, komu pomagają i czego potrzebują.</p>
-                <Switch>
-                    <Route path="/fundacje"> </Route>
-                    <Route path="/organizacje"> </Route>
-                    <Route path="/zbiorki"> </Route>
-                </Switch>
+                <ul className={"fundations__all"}>{
+                    organizations.map(el => (
+                        <section key={el.id} className={"fundations__list"}>
+                            <div className={"fundations__list-items"}>
+                                <li className={"fundations__list-items-name"}>{`${fundations} "${el.name}"`}</li>
+                                <li className={"fundations__list-items-target"}>{el.target}</li>
+                            </div>
+                            <li className={"fundations__list-things"}>{el.things}</li>
+                        </section>
+                    ))
+                }</ul>
             </div>
         </section>
     )
 }
+
