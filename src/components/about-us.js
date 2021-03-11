5fed358e2ba15e89c4104d9e454467a3b22e8f9c
Dominik <d.rolewski98@gmail.com>
2021-03-11 17:01:04 +0100

    settings container

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -4,14 +4,17 @@ import './aboutUs.scss'
 
 export const AboutUs = () => {
 
-    return(
+    return (
         <section className={"about"} id={"aboutUs"}>
-            <div className={"about__text"}>
-                <h3>O nas</h3>
-                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-            </div>
-            <div className={"about__image"}>
-                <img className={"about__img"} src={People} alt="people"/>
+            <div className={"about container"}>
+                <div className={"about__text"}>
+                    <h3>O nas</h3>
+                    <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts
+                        black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                </div>
+                <div className={"about__image"}>
+                    <img className={"about__img"} src={People} alt="people"/>
+                </div>
             </div>
         </section>
     )
