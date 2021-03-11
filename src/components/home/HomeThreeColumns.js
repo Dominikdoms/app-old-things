a8f550c2f5683718300f9355e4c952ce1e7c01d0
Dominik <d.rolewski98@gmail.com>
2021-03-11 17:25:08 +0100

    settings container

--- src/components/home/HomeThreeColumns.js
+++ src/components/home/HomeThreeColumns.js
@@ -10,22 +10,27 @@ export const HomeThreeColumns = () => {
     return (
         <>
             <section className={"home__tree-column"}>
-                <div className={"home__column"}>
-                    <h2 className={"home__count"}>{bags}</h2>
-                    <h3 className={"home__header"}>ODDANYCH WORKÓW</h3>
-                    <p className={"home__text"}>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
-                </div>
+                <div className="container">
+                    <div className={"home__column"}>
+                        <h2 className={"home__count"}>{bags}</h2>
+                        <h3 className={"home__header"}>ODDANYCH WORKÓW</h3>
+                        <p className={"home__text"}>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+                            enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
 
-                <div className={"home__column"}>
-                    <h2>{organizations}</h2>
-                    <h3>WSPARTYCH ORGANIZACJI</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
-                </div>
+                    <div className={"home__column"}>
+                        <h2>{organizations}</h2>
+                        <h3>WSPARTYCH ORGANIZACJI</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
+                            elementuma. Aliquam erat volutpat.</p>
+                    </div>
 
-                <div className={"home__column"}>
-                    <h2>{collections}</h2>
-                    <h3>ZORGANIZOWANY ZBIÓREK</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    <div className={"home__column"}>
+                        <h2>{collections}</h2>
+                        <h3>ZORGANIZOWANY ZBIÓREK</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
+                            elementuma. Aliquam erat volutpat.</p>
+                    </div>
                 </div>
             </section>
         </>
--- src/components/simpleSteps.js
+++ src/components/simpleSteps.js
@@ -9,30 +9,35 @@ export const SimpleSteps = () => {
             <h2 className={"steps__header"}>Wystarczą 4 proste kroki</h2>
 
             <div className={"steps__four"}>
-                <div className={"steps__one shirt"}>
-                    <p>Wybierz rzeczy</p>
-                    <span/>
-                    <p>ubrania, zabawki, sprzęt i inne</p>
-                </div>
-                <div className={"steps__one bag"}>
-                    <p>Spakuj je</p>
-                    <span/>
-                    <p>skorzystaj z worków na śmieci</p>
-                </div>
-                <div className={"steps__one magnifier"}>
-                    <p>Zdecyduj komu chcesz pomóc</p>
-                    <span/>
-                    <p>wybierz zaufane miejsce</p>
-                </div>
-                <div className={"steps__one movement"}>
-                    <p>Zamów kuriera</p>
-                    <span/>
-                    <p>kurier przyjedzie w dogodnym terminie</p>
+                <div className="container">
+                    <div className={"steps__one shirt"}>
+                        <p>Wybierz rzeczy</p>
+                        <span/>
+                        <p>ubrania, zabawki, sprzęt i inne</p>
+                    </div>
+                    <div className={"steps__one bag"}>
+                        <p>Spakuj je</p>
+                        <span/>
+                        <p>skorzystaj z worków na śmieci</p>
+                    </div>
+                    <div className={"steps__one magnifier"}>
+                        <p>Zdecyduj komu chcesz pomóc</p>
+                        <span/>
+                        <p>wybierz zaufane miejsce</p>
+                    </div>
+                    <div className={"steps__one movement"}>
+                        <p>Zamów kuriera</p>
+                        <span/>
+                        <p>kurier przyjedzie w dogodnym terminie</p>
+                    </div>
                 </div>
             </div>
             <div className={"steps__button"}>
-                <Link to="/logowanie"><button className={"steps__btn"}>ODDAJ <br/> RZECZ</button></Link>
+                <Link to="/logowanie">
+                    <button className={"steps__btn"}>ODDAJ <br/> RZECZ</button>
+                </Link>
             </div>
+
         </section>
     )
 }
