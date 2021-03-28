64eeb1689f6394b8da9d0d82082811d1d36e7182
Dominik <d.rolewski98@gmail.com>
2021-03-28 20:56:59 +0200

    three column created mobile

--- src/components/home/HomeThreeColumns.js
+++ src/components/home/HomeThreeColumns.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useState} from "react"
-// import "./homeThreeColumns.scss"
+import "./homeThreeColumns.scss"
 
 export const HomeThreeColumns = () => {
     const [bags, setBags] = useState("10")//fetch
