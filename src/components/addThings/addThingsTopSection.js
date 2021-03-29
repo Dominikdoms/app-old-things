a4e8d17c97ade89282c2bbc3e87de9c498026ef7
Dominik <d.rolewski98@gmail.com>
2021-03-29 20:26:08 +0200

    remove header

--- src/components/addThings/addThingsTopSection.js
+++ src/components/addThings/addThingsTopSection.js
@@ -4,7 +4,7 @@ import {
 } from 'react-router-dom';
 import {Link as LinkScroll, animateScroll as scroll} from "react-scroll";
 import clx from "classnames"
-// import './addThingsTopSection.scss';
+import './addThingsTopSection.scss';
 import sweater from "../../assets/Header-Form-Background.png";
 import Firebase from "../firebaseData";
 // import Firebase from "../firebaseData";
@@ -21,28 +21,6 @@ export const AddThingsTopSection = ({authUser, setAuthUser}) => {
             <div className={"add-things__container container"}>
 
                 <section className={"add-things__start"}>
-                    <ul className={clx({addThingsEmail: authUser}, "add-things__login")}>
-                        {authUser ? (
-                            <>
-                                <li>{authUser.email}</li>
-                                <li className={"navigation__login-start"}><Link to="/">Start</Link></li>
-                                <LinkScroll to="addThings" smooth={true} duration={500}>
-                                        <li className={"header__registration-btn"}>Oddaj rzeczy</li>
-                                </LinkScroll>
-                                <li onClick={handleLogout}><Link to="/wylogowano">wyloguj</Link></li>
-                            </>
-                        ) : (
-                            <>
-                                <li><Link to="/logowanie">Zaloguj</Link></li>
-                                <li className={"header__registration-btn"}><Link to="/rejestracja">Załóż konto</Link>
-                                </li>
-                            </>
-                        )}
-                    </ul>
-                    {/*<ul className={"add-things__navigation"}>*/}
-                    {/*    <li><Link to="/">Start</Link></li>*/}
-                    {/*</ul>*/}
-
                     <section className={"add-things__give-things"}>
                         <h1>Oddaj rzeczy, których już nie chcesz <br/> POTRZEBUJĄCYM</h1>
                         <h2>Wystarczą 4 proste kroki:</h2>
