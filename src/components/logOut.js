7bd2e1886a8f955ca9333a299afcdf24d195b1ad
Dominik <d.rolewski98@gmail.com>
2021-03-30 19:06:54 +0200

    rwd

--- src/components/logOut.js
+++ src/components/logOut.js
@@ -10,23 +10,6 @@ export const LogOut = () => {
     return (
         <nav className={"navigation"}>
             <div className={"navigation__container container"}>
-                <section className={"navigation__start"}>
-                    <ul className={"navigation__logout"}>
-                        <li><Link to="/logowanie">Zaloguj</Link></li>
-                        <li className={"navigation__registration-btn"}><Link to="/rejestracja">Załóż konto</Link></li>
-                    </ul>
-                    <ul className={"navigation__nav"}>
-                        <li><Link to="/" smooth={true} duration={1000}>Start</Link></li>
-                        {/*to="ID"*/}
-                        <li><LinkScroll to="steps" smooth={true} duration={1000}>O co chodzi?</LinkScroll></li>
-                        <li><LinkScroll to="aboutUs" smooth={true} duration={1000}>O nas</LinkScroll></li>
-                        <li><LinkScroll to="fundations" smooth={true} duration={1000}>Fundacja i
-                            Organizacje</LinkScroll></li>
-                        <li><LinkScroll to="contact" smooth={true} duration={1000}>Kontakt</LinkScroll></li>
-                    </ul>
-                </section>
-
-
                 <section className={"logging-out"}>
                     <header>
                         <h1>Wylogowanie nastąpiło pomyślnie</h1>
