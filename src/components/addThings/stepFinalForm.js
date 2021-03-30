f18e4c721436effa0d988d9c9ccec5d20d295ccf
Dominik <d.rolewski98@gmail.com>
2021-03-30 17:44:04 +0200

    created rwd

--- src/components/addThings/stepFinalForm.js
+++ src/components/addThings/stepFinalForm.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import './stepFinalForm.scss';
+import './stepFinalForm.scss';
 
 export const StepFinalForm = () => {
 
 
     return (
-        <section className={"steps__form"}>
-            <div className={"steps__form-container-final"}>
+        <section className={"final__form"}>
+            <div className={"final__form-container"}>
                 <div className={"steps-final"}>
                     <h1>Dziękujemy za przesłanie formularza Na maila prześlemy wszelkie informacje o odbiorze.</h1>
                 </div>
